748d59335889218dd546dcba48752f81609bd471
Dipesh Rajoria <dipeshrajoria45@gmail.com>
2024-02-24 05:45:22 +0530

    Update prompt.js

--- src/utils/prompt.js
+++ src/utils/prompt.js
@@ -224,18 +224,7 @@ const JSON_FORMAT = JSON.stringify({
   required: ["itinerary"],
 });
 
-const PROMPT = `You should always check if the place exist in the globe, if not then simply tell no place exist.
-User is going to place:PLACE, for number of days:NUMBER_OF_DAYS, for a this type of trip: TRIP_TYPE, user is
-starting the trip on TRIP_START, also the budget: BUDGET.
-Your task is to
-create a complete Trip Planner. Lets divide how you will give the trip planner step by step:
-You should always check if the place exist in the globe, if not then simply tell no place exist
-1.) First check if the place exists where the user is wanting to go, if not then tell the user that this does not present in globe !,
-Then check where the user is going, for how many days and which type of trip the user is planning, about trip start and budget. Always be customised and personalised for the user.
-2.) Take time to think and plan the trip on your own first.
-3.) You should provide where the user will go on which day, on a single day time based trip planning, at what time user will visit at
-what site seeing area, also add a small comment about that area, where the user will have breakfast, shopping, dinner etc.
-4.) Final Output is the json in the mentioned format:FORMAT. No extra text needed.`;
+const PROMPT = `write your prompt here`
 
 export const createTripPrompt = ({
   where_to,
@@ -244,13 +233,7 @@ export const createTripPrompt = ({
   trip_start = "",
   budget = "",
 }) => {
-  console.log({
-    where_to,
-    number_of_days,
-    itinerary_type,
-    trip_start,
-    budget,
-  });
+
   let prompt =
     `Strictly adhere to the mentioned JSON format` +
     `FORMAT:${JSON_FORMAT},` +
@@ -259,7 +242,6 @@ export const createTripPrompt = ({
     ` TRIP_START: ${trip_start} and` +
     `BUDGET:${budget}.` +
     PROMPT;
-
-  console.log({ prompt });
+  
   return prompt;
 };
